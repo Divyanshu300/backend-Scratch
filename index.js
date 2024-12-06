aafaf81fbb9847867693cbcf434d4dea402d8dd2
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-12-06 21:55:49 +0530

    learned using express

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const http = require("http"); //ye node mein already installed hota hai
-const { url } = require("inspector");
+// const http = require("http"); //ye node mein already installed hota hai
+// const { url } = require("inspector");
 
-const server = http.createServer((req , res) => { //jb bhi server ko access krenge to ek function call hoga
-    if(req.url === "/about") { //req.url mtlb "localhost:300" ke baad kyaa likha hai like localhost:3000/about {route}
-        res.end("The about page");//iss url prr ye text display ho jayega  
-    }
+// const server = http.createServer((req , res) => { //jb bhi server ko access krenge to ek function call hoga
+//     if(req.url === "/about") { //req.url mtlb "localhost:300" ke baad kyaa likha hai like localhost:3000/about {route}
+//         res.end("The about page");//iss url prr ye text display ho jayega  
+//     }
 
-    if(req.url === "/profile") {
-        res.end("The profile page")
-    }
+//     if(req.url === "/profile") {
+//         res.end("The profile page")
+//     }
     
-    if(req.url === "/") {
-        res.end("The home page")
-    }
-});
+//     if(req.url === "/") {
+//         res.end("The home page")
+//     }
+// });
 
-server.listen(3000);//server tb hi kaam krega jb usko listen krenge
+// server.listen(3000);//server tb hi kaam krega jb usko listen krenge
+
+//THE ABOVE METHOD IS NOT USED FOR PRODUCTION INSTEAD EXPRESS IS USED FOR PRODUCTION
+
+const express = require("express"); //EXPRESS EK PACKED TOOL BOX HAI
+const app = express(); //ISS LINE KI MADAD SE HMLOGO NE USKO UNPACK KRR DIY HAI AND SAARE CHEEZE JO USKE ANDAR HAI ISKE THROUGH USE KRR LETE HAI
+
+//EXPRESS BHI HTTP KA HI USE KRTA HAI
+
+app.get("/" , (req , res)=> { //IF ELSE LAGANE KE JAGAH WE CAN USE THESE
+    res.send("The home page"); //res.send === res.end
+})
+
+app.get("/about" , (req , res)=> { //IF ELSE LAGANE KE JAGAH WE CAN USE THESE
+    res.send("The about page"); //res.send === res.end
+})
+
+app.get("/profile" , (req , res)=> { //IF ELSE LAGANE KE JAGAH WE CAN USE THESE
+    res.send("The profile page"); //res.send === res.end
+})
+
+
+app.listen(3000);
 
