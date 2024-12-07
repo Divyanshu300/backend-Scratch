50f199041e5c168949dacc27b0301944705c949a
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-12-07 17:29:29 +0530

    created ui to upload file

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 require("dotenv").config();
 const connectToDB = require("./config/db");
 const userRoutes = require("./routes/user");
+const indexRoutes = require("./routes/index");
+const cookieParser = require("cookie-parser");
 connectToDB();
 
 app.set("view engine" , "ejs");//EK FOLDER BNAA DO ISKE BAAD view KE NAAM SE 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({extended : true}));
 
 //YHAA PRR KOI BHI ROUTE NHI LIKHTE PRODUCTION MEIN
@@ -14,6 +17,7 @@ app.use(express.urlencoded({extended : true}));
 //     res.render("index")
 // });
 
+app.use("/" , indexRoutes);//THE ROUTE WILL START WITH /{WHATEVER ROUTE WE WANT TO USE}
 app.use("/user" , userRoutes);//THE ROUTE WILL START WITH /user/{WHATEVER ROUTE WE WANT TO USE}
 
 app.listen(3000 , () => {
--- routes/user.js
+++ routes/user.js
@@ -43,5 +43,56 @@ router.post("/register" ,
     }
 );
 
+router.get("/login" , (req , res) => {
+    res.render("login");
+})
+
+router.post("/login" , 
+    body("username").trim().isLength({min : 3}),
+    body("password").trim().isLength({min : 5}),
+    async(req , res) => {
+        
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                error : errors.array(),
+                message : "Invalid Details"
+            });
+        } 
+        
+        const {username , password} = req.body;
+
+        const user = await userModel.findOne({
+            username : username
+        });
+
+        if(!user) {
+            return res.status(404).json({
+                message : "username or password incorrect"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password , user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({
+                message : "username or password incorrect"
+            })
+        }
+
+        const token = jwt.sign({
+                userId : user._id,
+                email : user.email,
+                username : user.username,
+            },
+            process.env.JWT_SECRET
+        );
+
+        //COOKIE USE KRNE KE LIYE cookie-parser KA USKE KRTE HAI 3rd PARTY MIDDLEWARE
+        //BROWSER AGAR KUCH BHI REQUEST KRTA HAI SERVER SE TOH YE COOKIE USS REQUEST KE SAATH JATI HAI
+        res.cookie("token" , token);//HMESHA TOKEN KO COOKIE MEIN SAVE KRTE HAI COOKIE ISSILIYE BNTI HAI
+        res.send("Logged In");
+    }
+);
 
 module.exports = router;
